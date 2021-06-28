2e0eb75999b464dc892367599316c4850e101a7a
unknown <sivaranjanib99@gmail.com>
2021-06-28 21:18:10 +0530

    welcome

---
+++ linecomparisionproblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linecomparisionproblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line comparison computation program");
+        }
+    }
+}
